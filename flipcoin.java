8fc47f59966a87a4debd09758c0349fc83407ea0
origino <aaaaaaaaaaaa056@gmail.com>
2022-07-14 22:15:33 +0900

    User name added

--- flipcoin.java
+++ flipcoin.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 public class flipcoin {
         public static void main(String[] args) { 
         	int counttail=0;
         	int counthead=0;
+        	System.out.println("Who are you?");
+        	Scanner sc = new Scanner(System.in);
+        	String user= sc.next();
+        	System.out.println("Hello, " +user +"!");
         	System.out.println("Tossing a coin...");
         	for(int i =1;i<=3;i++) {
                 if (Math.random() < 0.5){
