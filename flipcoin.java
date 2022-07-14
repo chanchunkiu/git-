2187dbc76a745628a60bbc03ab05a82e800847af
origino <aaaaaaaaaaaa056@gmail.com>
2022-07-14 22:22:50 +0900

    Victory message added

--- flipcoin.java
+++ flipcoin.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 public class flipcoin {
         public static void main(String[] args) { 
         	int counttail=0;
         	int counthead=0;
+        	System.out.println("Who are you?");
+        	Scanner sc = new Scanner(System.in);
+        	String user= sc.next();
+        	System.out.println("Hello, " +user +"!");
         	System.out.println("Tossing a coin...");
         	for(int i =1;i<=3;i++) {
                 if (Math.random() < 0.5){
@@ -13,5 +18,9 @@ public class flipcoin {
                 }
                 
         }
-        	System.out.println("Heads: " +counthead + " Tails: "+counttail);}
+        	System.out.println("Heads: " +counthead + " Tails: "+counttail);
+        	if(counthead>counttail) {
+        		System.out.println("You won");}
+        		else{System.out.println("You lost");}
+        	}
 }
